ecf0a2c677a799b1adef8270885e1431f54740de
guddu <guddupaul145@gmail.com>
2024-07-18 18:29:55 +0530

    working: animation and side bar

--- src/components/PageTransition.jsx
+++ src/components/PageTransition.jsx
@@ -3,13 +3,22 @@ import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
 const PageTransition = ({ children }) => {
-    const pathName = usePathname();
-  return <AnimatePresence>
-    <div key={pathName}>
-<div></div>
-{children}
-    </div>
-    </AnimatePresence>;
+  const pathName = usePathname();
+  return (
+    <AnimatePresence>
+      <div key={pathName}>
+        <motion.div
+          initial={{ opacity: 1 }}
+          animate={{
+            opacity: 0,
+            transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+          }}
+          className="h-screen w-screen fixed bg-primary "
+        />
+        {children}
+      </div>
+    </AnimatePresence>
+  );
 };
 
 export default PageTransition;
