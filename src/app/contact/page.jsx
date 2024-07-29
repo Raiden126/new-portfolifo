9e06b318710b43d801c82a573d896829c4067df5
guddu <guddupaul145@gmail.com>
2024-07-29 10:21:57 +0530

    ongoing: contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,21 @@
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
+
 const Contact = () => {
-    return (
-      <div>Contact page</div>
-    )
-  }
-  
-  export default Contact
+  return (
+    <Card
+      isBlurred
+      className="flex flex-col lg:flex-row h-auto w-4/5 mx-auto bg-gray-500"
+      shadow="sm"
+    >
+      <div className="basis-1/2">
+        <CardHeader className="text-3xl">Send Message</CardHeader>
+          <CardBody className="text-white">It's going to be a form</CardBody>
+      </div>
+      <div className="basis-1/2">
+        <CardHeader className="text-3xl">Get In Touch</CardHeader>
+      </div>
+    </Card>
+  );
+};
+
+export default Contact;
